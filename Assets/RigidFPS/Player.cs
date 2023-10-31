96d5611b942d917d0a08959be969a67fe305e203
Egor Persov <egor.persov@deuscraft.com>
2023-11-01 01:02:09 +1300

    More pre-jam goodness

--- Assets/RigidFPS/Player.cs
+++ Assets/RigidFPS/Player.cs
@@ -8,36 +8,13 @@ namespace RigidFps
 	[RequireComponent(typeof(CapsuleCollider), typeof(Rigidbody), typeof(PlayerInputHandler) )]
 	public class Player : MonoBehaviour
 	{
-		[Header("References")] [Tooltip("Reference to the main camera used for the player")]
+		[Header("References")] 
+		public Transform rotationRoot;
+		[Tooltip("Reference to the main camera used for the player")]
 		public Camera PlayerCamera;
 
 		[Tooltip("Audio source for footsteps, jump, etc...")]
 		public AudioSource AudioSource;
-
-		[Header("General")] [Tooltip("Force applied downward when in the air")]
-		public float GravityDownForce = 20f;
-
-		[Tooltip("Physic layers checked to consider the player grounded")]
-		public LayerMask GroundCheckLayers = -1;
-
-		[Tooltip("distance from the bottom of the character controller capsule to test for grounded")]
-		public float GroundCheckDistance = 0.05f;
-
-		[Header("Movement")] [Tooltip("Max movement speed when grounded (when not sprinting)")]
-		public float MaxSpeedOnGround = 10f;
-
-		[Tooltip(
-			"Sharpness for the movement when grounded, a low value will make the player accelerate and decelerate slowly, a high value will do the opposite")]
-		public float MovementSharpnessOnGround = 15;
-
-		[Tooltip("Max movement speed when not grounded")]
-		public float MaxSpeedInAir = 10f;
-
-		[Tooltip("Acceleration speed when in the air")]
-		public float AccelerationSpeedInAir = 25f;
-
-		[Tooltip("Multiplicator for the sprint speed (based on grounded speed)")]
-		public float SprintSpeedModifier = 2f;
 
 		[Tooltip("Height at which the player dies instantly when falling off the map")]
 		public float KillHeight = -50f;
@@ -45,23 +22,8 @@ namespace RigidFps
 		[Header("Rotation")] [Tooltip("Rotation speed for moving the camera")]
 		public float RotationSpeed = 200f;
 
-		[Range(0.1f, 1f)] [Tooltip("Rotation speed multiplier when aiming")]
-		public float AimingRotationMultiplier = 0.4f;
-
-		[Header("Jump")] [Tooltip("Force applied upward when jumping")]
-		public float JumpForce = 9f;
-
-		[Header("Stance")] [Tooltip("Ratio (0-1) of the character height where the camera will be at")]
-		public float CameraHeightRatio = 0.9f;
-
-		[Tooltip("Height of character when standing")]
-		public float CapsuleHeightStanding = 1.8f;
-
 		[Header("Audio")] [Tooltip("Amount of footstep sounds played when moving one meter")]
 		public float FootstepSfxFrequency = 1f;
-
-		[Tooltip("Amount of footstep sounds played when moving one meter while sprinting")]
-		public float FootstepSfxFrequencyWhileSprinting = 1f;
 
 		[Tooltip("Sound played for footsteps")]
 		public AudioClip FootstepSfx;
@@ -72,25 +34,6 @@ namespace RigidFps
 		[Tooltip("Sound played when taking damage froma fall")]
 		public AudioClip FallDamageSfx;
 
-		[Header("Fall Damage")]
-		[Tooltip("Whether the player will recieve damage when hitting the ground at high speed")]
-		public bool RecievesFallDamage;
-
-		[Tooltip("Minimun fall speed for recieving fall damage")]
-		public float MinSpeedForFallDamage = 10f;
-
-		[Tooltip("Fall speed for recieving th emaximum amount of fall damage")]
-		public float MaxSpeedForFallDamage = 30f;
-
-		[Tooltip("Damage recieved when falling at the mimimum speed")]
-		public float FallDamageAtMinSpeed = 10f;
-
-		[Tooltip("Damage recieved when falling at the maximum speed")]
-		public float FallDamageAtMaxSpeed = 50f;
-
-		[Tooltip("CharacterController.slopeLimit")]
-		public float slopeLimit = 45f;
-		
 		[Header("Physics movement")]
 		public float maxMovementSpeed = 10.0f;
 		public float movementAccel = 20.0f;
@@ -152,41 +95,67 @@ namespace RigidFps
 			// landing
 			if (IsGrounded && !wasGrounded)
 			{
-				// Fall damage
-				float fallSpeed = -Mathf.Min(CharacterVelocity.y, latestImpactSpeed.y);
-				float fallSpeedRatio = (fallSpeed - MinSpeedForFallDamage) /
-									   (MaxSpeedForFallDamage - MinSpeedForFallDamage);
-				if (RecievesFallDamage && fallSpeedRatio > 0f)
-				{
-					float dmgFromFall = Mathf.Lerp(FallDamageAtMinSpeed, FallDamageAtMaxSpeed, fallSpeedRatio);
-					//m_Health.TakeDamage(dmgFromFall, null);
-
-					// fall damage SFX
-					AudioSource.PlayOneShot(FallDamageSfx);
-				}
-				else
-				{
-					// land SFX
-					AudioSource.PlayOneShot(LandSfx);
-				}
+				// land SFX
+				AudioSource.PlayOneShot(LandSfx);
 			}
 
-			HandleCharacterMovement( Time.deltaTime );
+			if( jumpTimeout > 0.0f )
+				jumpTimeout -= Time.deltaTime;
+			if( groundCheck.IsGrounded && inputHandler.GetJumpInputDown() && jumpTimeout <= 0.0f )
+				doJump = true;
+
+			if( IsGrounded )
+			{
+				// footsteps sound
+				float chosenFootstepSfxFrequency = FootstepSfxFrequency;
+				if (footstepDistanceCounter >= 1f / chosenFootstepSfxFrequency)
+				{
+					footstepDistanceCounter = 0f;
+					AudioSource.PlayOneShot(FootstepSfx);
+				}
+
+				// keep track of distance traveled for footsteps sound
+				footstepDistanceCounter += rigidbody.velocity.magnitude * Time.deltaTime;
+			}
+
+			//HandleCharacterRotation( Time.deltaTime );
+
+			// converts move input to a worldspace vector based on our character's transform orientation
+			worldspaceMoveInput = rotationRoot.TransformVector(inputHandler.GetMoveInput());
 		}
 
+		void FixedUpdate()
+		{
+			HandleCharacterMovement( Time.fixedDeltaTime );
+		}
+
+		void LateUpdate()
+		{
+			HandleCharacterRotation( Time.deltaTime );
+		}
+
+		Vector3 worldspaceMoveInput;
+		bool doJump;
+		float jumpTimeout;
 		Vector3 desiredVelocity;
 		Vector3 horizontalAccel;
 		Vector3 velDiff;
 		float maxAccelMagnitude;
 
+
+		public float followSmoothTime = 0.2f;
+		public float cameraHeight = 1.5f;
+		Vector3 cameraVelocity;
 		void HandleCharacterRotation( float dt )
 		{
+			float bearing;
 			// horizontal character rotation
 			{
 				// rotate the transform with the input speed around its local Y axis
-				transform.Rotate(
-					new Vector3(0f, (inputHandler.GetLookInputsHorizontal() * RotationSpeed * RotationMultiplier),
-						0f), Space.Self);
+				rotationRoot.Rotate(
+					new Vector3(0f, inputHandler.GetLookInputsHorizontal() * RotationSpeed * RotationMultiplier, 0f)
+					, Space.Self);
+				bearing = rotationRoot.eulerAngles.y;
 			}
 
 			// vertical camera rotation
@@ -198,82 +167,46 @@ namespace RigidFps
 				cameraVerticalAngle = Mathf.Clamp(cameraVerticalAngle, -89f, 89f);
 
 				// apply the vertical angle as a local rotation to the camera transform along its right axis (makes it pivot up and down)
-				PlayerCamera.transform.localEulerAngles = new Vector3(cameraVerticalAngle, 0, 0);
+				PlayerCamera.transform.localEulerAngles = new Vector3(cameraVerticalAngle, bearing, 0);
+
+				PlayerCamera.transform.position = Vector3.SmoothDamp(
+					PlayerCamera.transform.position,
+					rotationRoot.position + new Vector3( 0.0f, cameraHeight, 0.0f),
+					ref cameraVelocity,
+					followSmoothTime,
+					Mathf.Infinity,
+					dt
+				);
 			}
 		}
 
 		void HandleCharacterMovement( float dt )
 		{
-			// character movement handling
-			bool isSprinting = inputHandler.GetSprintInputHeld();
+			// figure out horizontal acceleration to be applied
+			var horizontalVelocity = Vector3.ProjectOnPlane( rigidbody.velocity, Vector3.up );
+
+			desiredVelocity = worldspaceMoveInput * maxMovementSpeed;
+			velDiff = desiredVelocity - horizontalVelocity;
+
+			maxAccelMagnitude = Mathf.Min( velDiff.magnitude / dt, IsGrounded ? movementAccel : airAccel );
+			horizontalAccel = velDiff.normalized * maxAccelMagnitude;
+
+			rigidbody.AddForce( horizontalAccel, ForceMode.Acceleration );
+
+			// jumping
+			if ( doJump)
 			{
-				float speedModifier = isSprinting ? SprintSpeedModifier : 1f;
+				doJump = false;
+				jumpTimeout = k_JumpGroundingPreventionTime;
+				rigidbody.AddForce( Vector3.up * jumpForce, ForceMode.VelocityChange );
 
-				// converts move input to a worldspace vector based on our character's transform orientation
-				Vector3 worldspaceMoveInput = transform.TransformVector(inputHandler.GetMoveInput());
+				// play sound
+				AudioSource.PlayOneShot(JumpSfx);
 
-				// figure out horizontal acceleration to be applied
-				var velocity = rigidbody.velocity;
-				var horizontalVelocity = Vector3.ProjectOnPlane( rigidbody.velocity, Vector3.up );
-
-				desiredVelocity = worldspaceMoveInput * maxMovementSpeed;
-				velDiff = desiredVelocity - horizontalVelocity;
-
-				maxAccelMagnitude = Mathf.Min( velDiff.magnitude / dt, IsGrounded ? movementAccel : airAccel );
-				horizontalAccel = velDiff.normalized * maxAccelMagnitude;
-
-				rigidbody.AddForce( horizontalAccel, ForceMode.Acceleration );
-
-				// jumping
-				if (IsGrounded && inputHandler.GetJumpInputDown())
-				{
-					rigidbody.AddForce( Vector3.up * jumpForce, ForceMode.VelocityChange );
-
-					// play sound
-					AudioSource.PlayOneShot(JumpSfx);
-
-					// remember last time we jumped because we need to prevent snapping to ground for a short time
-					lastTimeJumped = Time.time;
-					HasJumpedThisFrame = true;
-
-					// Force grounding to false
-					IsGrounded = false;
-					groundNormal = Vector3.up;
-				}
-
-				if( IsGrounded )
-				{
-					// footsteps sound
-					float chosenFootstepSfxFrequency =
-						isSprinting ? FootstepSfxFrequencyWhileSprinting : FootstepSfxFrequency;
-					if (footstepDistanceCounter >= 1f / chosenFootstepSfxFrequency)
-					{
-						footstepDistanceCounter = 0f;
-						AudioSource.PlayOneShot(FootstepSfx);
-					}
-
-					// keep track of distance traveled for footsteps sound
-					footstepDistanceCounter += CharacterVelocity.magnitude * Time.deltaTime;
-				}
+				// Force grounding to false
+				IsGrounded = false;
+				groundNormal = Vector3.up;
 			}
-/*
-			// apply the final calculated velocity value as a character movement
-			Vector3 capsuleBottomBeforeMove = GetCapsuleBottomHemisphere();
-			Vector3 capsuleTopBeforeMove = GetCapsuleTopHemisphere();
-			//controller.Move(CharacterVelocity * Time.deltaTime);
-
-			// detect obstructions to adjust velocity accordingly
-			latestImpactSpeed = Vector3.zero;
-			if (Physics.CapsuleCast(capsuleBottomBeforeMove, capsuleTopBeforeMove, collider.radius,
-				CharacterVelocity.normalized, out RaycastHit hit, CharacterVelocity.magnitude * Time.deltaTime, -1,
-				QueryTriggerInteraction.Ignore))
-			{
-				// We remember the last impact speed because the fall damage logic might need it
-				latestImpactSpeed = CharacterVelocity;
-
-				CharacterVelocity = Vector3.ProjectOnPlane(CharacterVelocity, hit.normal);
-			}
-*/
 		}
 
 		void OnDie()
