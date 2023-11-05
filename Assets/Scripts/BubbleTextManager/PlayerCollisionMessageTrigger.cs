acd91e6465a12ff213075064f1d9e1a7a13cbe25
Koniakhin Sergey <sakonyakhin@asuproject.ru>
2023-11-05 14:10:03 +0700

    Asset type for TextSpeech

--- Assets/Scripts/BubbleTextManager/PlayerCollisionMessageTrigger.cs
+++ Assets/Scripts/BubbleTextManager/PlayerCollisionMessageTrigger.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
+using SpeechText;
+using System.Collections.Generic;
+using System.Linq;
 
 public class TriggerTest : MonoBehaviour
 {
     public string message = "Sample Text";
+    public TextData textDataAsset;
+
+    public string currentState = "Default";
     private BubbleTextManager bubbleTextManager;
 
     private void Start()
@@ -17,12 +23,12 @@ public class TriggerTest : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-
+        var textLines = textDataAsset.GetTextLinesByStateName(currentState);
         if (bubbleTextManager != null)
         {
-            int randomNumber = Random.Range(1, 101);
-            string randomString = randomNumber.ToString();
-            bubbleTextManager.OnAddMessageToQueue(new BubbleTextMessage { messageText = randomString, showDuration = 2, target = gameObject, messageIntonation = MessageIntonations.Cry });
+            bubbleTextManager.OnAddMessageToQueue(
+                new BubbleTextMessage { messageText = textLines[Random.Range(0, textLines.Count)].text, showDuration = 2, target = gameObject }
+                );
         }
     }
 
---
+++ Assets/SpeechText/TextData.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace SpeechText
+{
+    [Serializable]
+    public class TextLine
+    {
+        public string text;
+    }
+
+    [Serializable]
+    public class TextSection
+    {
+        public string StateName;
+        public List<TextLine> lines = new List<TextLine>();
+    }
+
+    [CreateAssetMenu(fileName = "New Text Data", menuName = "Custom/Text Data")]
+    public class TextData : ScriptableObject
+    {
+        public List<TextSection> states = new List<TextSection>();
+        public float messageShowCooldown = 3;
+
+        public List<TextLine> GetTextLinesByStateName(string stateName)
+        {
+            return states.Find(item => item.StateName == "Default")?.lines ?? new List<TextLine>();
+        }
+    }
+}
