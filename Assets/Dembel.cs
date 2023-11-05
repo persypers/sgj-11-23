e208f404d60d486e8cd5f722a63242afe629dc45
Egor Persov <egor.persov@deuscraft.com>
2023-11-06 02:20:37 +1300

    GrabItemBehaviour

--- Assets/Dembel.cs
+++ Assets/Dembel.cs
@@ -2,55 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dembel : MonoBehaviour
+public class Dembel : GrabItemBehaviour
 {
-	public ConfigurableJoint guitarJoint;
-	public ItemTrigger guitarTrigger;
 	public AudioSource guitarSongSource;
 	public AudioSource guitarPickupSource;
-	ConfigurableJoint joint;
-	Coroutine guitarCoro;
-	void Start()
-	{
-		guitarTrigger.actionWithItem.AddListener( PlayGuitar );
-	}
 
-	public void PlayGuitar( Item item )
+	override public IEnumerator DoBehaviour( Item item )
 	{
-		if( !joint )
-		{
-			joint = Fancy.Helpers.CopyComponent( guitarJoint, item.gameObject );
-			item.OnPickUp.AddListener( DropGuitar );
-			item.OnItemJointBreak.AddListener( DropGuitar );
-			if( guitarCoro != null)
-				StopCoroutine( guitarCoro );
-			guitarCoro = StartCoroutine( GuitarCoroutine( item ) );
-		}
-	}
+		yield return base.DoBehaviour( item );		// ждём пока предмет подлетит
 
-	void DropGuitar( Item guitar )
-	{
-		if( guitarCoro != null)
-			StopCoroutine( guitarCoro );
-		guitarSongSource.Stop();
-		var item = joint.GetComponent< Item >();
-		item.OnPickUp.RemoveListener( DropGuitar );
-		item.OnItemJointBreak.RemoveListener( DropGuitar );
-		GameObject.Destroy( joint );
-	}
-
-	IEnumerator GuitarCoroutine( Item guitar )
-	{
-		while( ( guitar.transform.position - transform.position ).sqrMagnitude > 3.0f )
-		{
-			yield return null;
-		}
 		guitarPickupSource.Play();
 		guitarSongSource.Play();
 	}
 
-	public void TestFunction()
+	override public void OnBehaviourCanceled( Item item )
 	{
-		Debug.Log( "PATEFON PRINESLI");
+		guitarSongSource.Stop();
 	}
 }
