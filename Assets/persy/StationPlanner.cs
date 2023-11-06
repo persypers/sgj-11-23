b9b3b5092c8ccf836f4ae7ef55e885499bcc8693
Ivan Smal <vanasmal@mail.ru>
2023-11-06 21:33:18 +0700

    Station Generation

--- Assets/persy/StationPlanner.cs
+++ Assets/persy/StationPlanner.cs
@@ -6,11 +6,15 @@ public class StationPlanner : Fancy.MonoSingleton< StationPlanner >
 {
 	public float coalFuelOnBoard = 0;
 	public float totalFuelOnBoard = 0;
-	public Fancy.ObjectPool stationGenerator;
+
+	public List<Fancy.ObjectPool> stationGenerators;
+	
+	//public Fancy.ObjectPool stationGenerator;
 
 	public GameObject SpawnStation( TileData tile )
 	{
-		var go = stationGenerator.Get();
+		int random = Random.Range(0, stationGenerators.Count);
+		var go = stationGenerators[random].Get();
 		go.transform.position = tile.transform.position;
 		tile.Add( go );
 
