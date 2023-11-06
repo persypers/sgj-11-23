3a5762d7b87add718e9fadb33ed47c6586db2910
Egor Persov <egor.persov@deuscraft.com>
2023-11-06 21:23:38 +1300

    Burn particles

--- Assets/Scripts/Furnace.cs
+++ Assets/Scripts/Furnace.cs
@@ -1,7 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Burst;
+using Fancy;
 using UnityEngine;
 
 public class Furnace : Fancy.MonoSingleton< Furnace >
@@ -14,6 +13,9 @@ public class Furnace : Fancy.MonoSingleton< Furnace >
 	
 	public float maxAudioAtFuel = 5.0f;
 	public float audioBumpPerItem = 0.3f;
+
+	public ObjectPool particles;
+	public int particlesOnBurn = 3;
 
 	List< Item > items = new List< Item >();
 
@@ -63,7 +65,31 @@ public class Furnace : Fancy.MonoSingleton< Furnace >
 
 	private void Consume( Item item )
 	{
+		var col = item.GetComponent< Collider >();
+		var bounds = col == null ? new Bounds( item.transform.position, new Vector3( 0.5f, 0.5f, 0.5f ) ) : item.GetComponent< Collider >().bounds;
+		for( int i = 0; i < particlesOnBurn; i++ )
+		{
+			var go = particles.Get();
+			Vector3 pos = new Vector3(
+				Random.Range( bounds.min.x, bounds.max.x ),
+				Random.Range( bounds.min.y, bounds.max.y ) - 0.4f,
+				Random.Range( bounds.min.z, bounds.max.z ) );
+			go.transform.position = pos;
+			go.transform.localRotation = Quaternion.Euler(
+				Random.Range( -15.0f, 15.0f ),
+				Random.Range( -5.0f, 5.0f ),
+				Random.Range( 0.0f, 360.0f ) );
+			go.SetActive( true );
+			go.GetComponentInChildren< ParticleSystem >().Play();
+			StartCoroutine( DisableParticles( go ) );
+		}
 		fuel += item.fuelValue;
 		GameObject.Destroy( item.gameObject );
+	}
+
+	IEnumerator DisableParticles( GameObject bo )
+	{
+		yield return new WaitForSeconds( 2.0f );
+		bo.SetActive( false );
 	}
 }
